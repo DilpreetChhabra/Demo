0483c6a5908816259ae17a5ecfedce4f01990c47
Dilpreet Chhabra <162965126+DilpreetChhabra@users.noreply.github.com>
2024-04-05 15:54:31 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,43 @@
+# example of Integer
+# a=5
+# print(type(a))
+
+# example of float
+# a=5.5
+# print(type(a))
+
+# example of complex number
+# a=4+2j
+# print(type(a))
+
+# EXAMPLE OF COMPLEX INVALID AS I IS NOT ALLOWED
+# a=2+4i
+# print(type(a))
+
+# boolean example both true and false are type of boolean
+# a=True
+# print(type(a))
+
+# Example of string can use single quote double or triple quote
+# string_1='Welcome to the python world'
+# print(type(string_1))
+
+# example of list
+# a=[1,3,'hello',5,'name']
+# print(type(a))
+
+# example of tuple
+# tuple1=(1,3,'hello',5,'name')
+# print(type(tuple1))
+
+# example fo set
+# set1={1,3,'hello',5,'name'}
+# print(type(set1))
+
+# example of dictionary
+# dict1={"name":"priya","Age":"25","city":"Dewas"}
+# print(type(dict1))
+
+# To find particular value from dictionary set
+# dict1={"name":"priya","Age":"25","city":"Dewas"}
+# print(dict1["Age"])
